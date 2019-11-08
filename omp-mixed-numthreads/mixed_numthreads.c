18a89a963a5cc86e8dc4468b305af0575b4f9f9e
Fabio Luporini <fabilupo@gmail.com>
2019-11-08 03:58:27 -0600

    mixed-parallelism -> mixed-numthreads

---
+++ omp-mixed-numthreads/mixed_numthreads.c
@@ -0,0 +1,67 @@
+#define _GNU_SOURCE // sched_getcpu(3) is glibc-specific (see the man page)
+
+#include <omp.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+
+int main(int argc, char* argv[])
+{
+  if (argc != 3)
+  {
+    printf("Usage: nested num_threads_fst num_threads_snd\n");
+    return 0;
+  }
+
+  const int num_threads_fst = atoi(argv[1]);
+  const int num_threads_snd = atoi(argv[2]);
+
+  // Systematically disable dynamic adjustment of the number of threads
+  // available for running parallel regions (the default value is
+  // otherwise implementation defined)
+  omp_set_dynamic(0);
+
+  // To do some computation
+  int array_fst[num_threads_fst];
+  int array_snd[num_threads_snd];
+
+  // Timers
+  struct timeval start, end;
+
+  // Computation
+  const int nsteps = 100000;
+  printf("Now computing %d steps\n", nsteps);
+  gettimeofday(&start, NULL);
+  for (int n = 0; n < nsteps; n++)
+  {
+    #pragma omp parallel for num_threads(num_threads_fst)
+    for (int i = 0; i < num_threads_fst; i++)
+    {
+      array_fst[i] = num_threads_fst*num_threads_snd;
+    }
+
+    #pragma omp parallel for num_threads(num_threads_snd)
+    for (int i = 0; i < num_threads_snd; i++)
+    {
+      array_snd[i] = num_threads_fst*num_threads_snd;
+    }
+  }
+  gettimeofday(&end, NULL);
+
+  // Avoid loops are optimized away
+  int sum = 0;
+  for (int i = 0; i < num_threads_fst; i++)
+  {
+    for (int j = 0; j < num_threads_snd; j++)
+    {
+      sum += array_fst[i] + array_snd[j];
+    }
+  }
+
+  double elapsed = (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec)/1000000;
+  printf("Computed <%d> in %f secs\n", sum, elapsed);
+
+  return(0);
+}
