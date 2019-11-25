3e0622ad20546e5c17fd9b3ebbe43da4f9ee619a
Fabio Luporini <fabilupo@gmail.com>
2019-11-25 08:59:14 -0600

    Add omp-mixed-parallelism program

---
+++ omp-mixed-parallelism/README.md
@@ -0,0 +1,11 @@
+### Example compilation
+
+```
+CC=icc CFLAGS=-qopenmp make clean mixed_parallelism
+```
+
+### Example runs
+
+Assume you're running on a platform with 64 physical cores and hyperthreading enabled.
+
+First, try ...
---
+++ omp-mixed-parallelism/mixed_parallelism.c
@@ -0,0 +1,83 @@
+#define _GNU_SOURCE // sched_getcpu(3) is glibc-specific (see the man page)
+
+#include <omp.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+
+int main(int argc, char* argv[])
+{
+  if (argc != 4)
+  {
+    printf("Usage: nested num_threads_fst_outer num_threads_fst_inner num_threads_snd\n");
+    return 0;
+  }
+
+  const int num_threads_fst_outer = atoi(argv[1]);
+  const int num_threads_fst_inner = atoi(argv[2]);
+  const int num_threads_snd = atoi(argv[3]);
+
+  const int num_threads_fst = num_threads_fst_outer*num_threads_fst_inner;
+
+  // Systematically disable dynamic adjustment of the number of threads
+  // available for running parallel regions (the default value is
+  // otherwise implementation defined)
+  omp_set_dynamic(0);
+
+  // Maybe enable nested parallelism
+  if (num_threads_fst_inner > 1)
+  {
+    omp_set_nested(1);
+    omp_set_max_active_levels(2);
+  }
+
+  // To do some computation
+  const int padding = 10000;
+  int array_fst[num_threads_fst][padding];
+  int array_snd[num_threads_snd][padding];
+
+  // Timers
+  struct timeval start, end;
+
+  // Computation
+  const int nsteps = 100000;
+  printf("Now computing %d steps\n", nsteps);
+  gettimeofday(&start, NULL);
+  for (int n = 0; n < nsteps; n++)
+  {
+    int i;  
+    #pragma omp parallel for num_threads(num_threads_fst_outer) firstprivate(i)
+    for (i = 0; i < num_threads_fst_outer; i++)
+    {
+      #pragma omp parallel for num_threads(num_threads_fst_inner)
+      for (int j = 0; j < num_threads_fst_inner; j++)
+      {
+        array_fst[i*num_threads_fst_inner + j][0] = num_threads_fst*num_threads_snd;
+      }
+    }
+
+    #pragma omp parallel for num_threads(num_threads_snd)
+    for (int i = 0; i < num_threads_snd; i++)
+    {
+      array_snd[i][0] = num_threads_fst*num_threads_snd;
+    }
+  }
+  gettimeofday(&end, NULL);
+
+  // Avoid loops are optimized away
+  int sum = 0;
+  for (int i = 0; i < num_threads_fst; i++)
+  {
+    for (int j = 0; j < num_threads_snd; j++)
+    {
+      sum += array_fst[i][0] + array_snd[j][0];
+    }
+  }
+
+  double elapsed = (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec)/1000000;
+  printf("Computed <%d> in %f secs\n", sum, elapsed);
+
+  return(0);
+}
